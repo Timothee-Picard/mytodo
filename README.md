bd32268c49efbb29f10c1224c64bda4c0284a607
timothée picard <timothee.picard@outlook.fr>
2025-04-11 13:31:27 +0200

    feat: update reademe

--- README.md
+++ README.md
@@ -1,79 +1,45 @@
-# Introduction
+# Todo
+URL du site: https://ynovtodolist.osc-fr1.scalingo.io/
 
-This web app built with a CLEAN stack (CLoudant NoSQL DB, Express, Angular and Node.js) is ready to be deployed on ICP (IBM Cloud Platform).
+![img_5.png](images/img_5.png)
 
-![Todo](./images/screenshot.png)
+## Modification de db-mongo.js
+J'ai remplacer la connexion par username mot de passe par l'url
 
+![img.png](images/img.png)
 
-Watch this 6 mins <a href="https://youtu.be/XVVb-aLw9ow" target=”_blank”>YouTube video</a> to understand all the deployment steps below. Note: this video excludes the cluster provisioning.
+J'ai mis le bon nom de ma base de donnée 
 
+![img_2.png](images/img_2.png)
 
-# How to deploy this app in Kubernetes?
+## Modification de server.js
 
-1. If you don't already have a Kubernetes cluster, create one for **Free** from IBM Cloud Catalog by selecting the [Kubernetes Service](https://cloud.ibm.com/kubernetes/catalog/create).
+![img.png](images/img_1.png)
 
-    Give it a **Name** and select a **Resource Group**.
-    > 20 min provisioning time
+## Modification BDD Scalingo
+Activation du TLS et Block non-TLS connections
+Download Certificate
 
-    ![Cluster](./images/iks-free-cluster.jpg)
+![img_4.png](images/img_4.png)
 
-1. [Optional] If you want to securely store your API Key used in the Continuous Delivery later, provision a service [Key Protect](https://cloud.ibm.com/catalog/services/key-protect).
+## Ajout des variable d'env sur Scalingo
 
-    Make sure to select the same **Region** as your cluster location, enter a **Service Name**, select a **Resource Group** and a **Network Policy**.
-    > 2 min provisioning time
+![img_3.png](images/img_3.png)
 
-    ![Key Protect](./images/key-protect.jpg)
+Pour récupérer le certificat je l'ai téléchargé puis ai utilisé:
 
-1. To automate the deployment of this app into your Kubernetes cluster, click the button below.
+``` sh
+base64 -i ca.pem
+```
 
-    <a href="https://cloud.ibm.com/devops/setup/deploy?repository=https://github.com/lionelmace/mytodo&branch=master" target=”_blank”>![](./images/toolchain0-button.png)</a>
+## Ajout des variable d'env sur Scalingo
+On créer dans un groupe de ressource une base de donée Cosmos
 
+![img_7.png](images/img_7.png)
 
-1. Enter a **Toolchain Name**, select the **Region** and a **Resource Group** where your cluster was created.
+et on vas modifier les var d'env de Scalingo on créer une variable AZUR_MONGO_URL
+On modifie MONGO_URL qui prendra maintenant $AZUR_MONGO_URL
 
-    ![Toolchain](./images/toolchain1-create.jpg)
+Dans AZUR_MONGO_URL on récupère l'url direct sur Azur
 
-1. In the tab **Git Repos and Issue Tracking**, keep the default setting .
-
-    ![Toolchain](./images/toolchain2-git.jpg)
-
-1. In the tab **Delivery Pipeline**, create a new API Key.
-
-    ![Toolchain](./images/toolchain3-newkey.jpg)
-
-1. A panel will open, check the option **Save this key in a secrets store for resuse** if you have created an instance of the service Key Protect.
-    > Keep this option unchecked if you have decided not to use Key Protect.
-
-    ![Toolchain](./images/toolchain4-secretkey.jpg)
-
-
-1. The toolchain will automatically try to fill out the remaining information. Control the Resource Group, the region and the cluster name, then, click **Create**. 
-
-    ![Toolchain](./images/toolchain5-final.jpg)
-
-1. The toolchain is being created. That includes a Github repo to clone the source code of the app. 
-
-    ![Toolchain](./images/toolchain6-overview.jpg)
-
-1. Click **Delivery Pipeline** in the Overview. You will see the stages progressing in pipeline.
-
-    > 6 min deployment time 
-    ![Toolchain](./images/toolchain7-pipeline.jpg)
-
-1. Click the link **View logs and history** in the last stage **DEPLOY**. Scroll down to the bottom. You will find the link to your application.
-
-    ![Toolchain](./images/toolchain8-applink.jpg)
-
-    > If you have a free cluster, the url will be using the IP address of a worker node.
-    > If you have a paid cluster, the url will be a domain name finishing with *.appdomain.cloud.
-
-Congratulations! Your app is up and running in the cluster.
-
-
-# Step by step Deployment
-
-Those two tutorials will show you in details how to deploy this step by step:
-
-* With IKS (IBM Cloud Kubernetes Service), follow this [tutorial](https://lionelmace.github.io/iks-lab)
-
-* With ICF (IBM Cloud Foundry), follow this [tutorial](https://github.com/lionelmace/ibmcloud-labs/tree/master/labs/Lab%20Cloud%20Foundry%20-%20Deploy%20TODO%20web%20application)
+![img_6.png](images/img_6.png)
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const appEnv = cfenv.getAppEnv(appEnvOpts);
 // Run locally - Load env variables from .env file
 const result = require('dotenv').config({
   path: __dirname + '/credentials.env'
-}); 
+});
 if (result.error) {
   //console.log('Cannot find credentials.env');
 } else {
@@ -55,10 +55,8 @@ if (result.error) {
 // Database ----------------------------------------------------------------
 let db;
 if (process.env.CLOUDANT_USERNAME !== undefined)  {
-  db = require('./lib/db-cloudant')(process.env);
-} else if (process.env.MONGO_USERNAME !== undefined) {
-  db = require('./lib/db-mongo')(process.env);
-} else if (process.env.MONGO_URL) {
+    db = require('./lib/db-cloudant')(process.env);
+} else if (process.env.MONGO_URL !== undefined) {
     db = require('./lib/db-mongo')(process.env);
 } else {
     db = require('./lib/in-memory')();
@@ -66,7 +64,7 @@ if (process.env.CLOUDANT_USERNAME !== undefined)  {
 console.log('Using', db.type());
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }))
 app.use(bodyParser.json()); // parse application/json
 
@@ -74,12 +72,12 @@ app.use(bodyParser.json()); // parse application/json
 /*
 // Enable reverse proxy support in Express. This causes the
 // the "X-Forwarded-Proto" header field to be trusted so its
-// value can be used to determine the protocol. See 
+// value can be used to determine the protocol. See
 // http://expressjs.com/api#app-settings for more details.
 app.enable('trust proxy');
 
-// Add a handler to inspect the req.secure flag (see 
-// http://expressjs.com/api#req.secure). This allows us 
+// Add a handler to inspect the req.secure flag (see
+// http://expressjs.com/api#req.secure). This allows us
 // to know whether the request was via http or https.
 app.use (function (req, res, next) {
   if (req.secure) {
@@ -97,13 +95,13 @@ app.use(favicon(__dirname + '/public/icons/favicon-check.ico'));
 
 // Healthcheck use for LivenessProbe ---------------------------------------
 app.get('/healthcheck',(req,res)=> {
-  res.send ("Health check passed");
- });
- app.get('/badhealth',(req,res)=> {
-     res.status(500).send('Health check did not pass');
- });
+    res.send ("Health check passed");
+});
+app.get('/badhealth',(req,res)=> {
+    res.status(500).send('Health check did not pass');
+});
 
-// Load test - will generate CPU stress for a few seconds. 
+// Load test - will generate CPU stress for a few seconds.
 app.get('/loadtest', function (req, res) {
     res.send("Load test complete: " + fibo(45));
 })
@@ -113,49 +111,49 @@ function fibo(n) {
         return 1;
     else return fibo(n - 2) + fibo(n - 1);
 }
- 
+
 // API ---------------------------------------------------------------------
 app.get('/api/todos', (req, res) => {
-  db.search().then(todos => {
-    res.send(todos);
-  }).catch(err => {
-    res.status(500).send({ error: err });
-  });
+    db.search().then(todos => {
+        res.send(todos);
+    }).catch(err => {
+        res.status(500).send({ error: err });
+    });
 });
 
 app.post('/api/todos', (req, res) => {
-  db.create({ text: req.body.text, completed: false})
-    .then(todo  => db.search())
-    .then(todos => res.send(todos))
-    .catch(err => { 
-      res.status(500).send({ error: err });
-  });
+    db.create({ text: req.body.text, completed: false})
+        .then(todo  => db.search())
+        .then(todos => res.send(todos))
+        .catch(err => {
+            res.status(500).send({ error: err });
+        });
 });
 
 app.get('/api/todos/:id', (req, res) => {
-  db.get(req.params.id).then(todo => {
-    res.send(todo);
-  }).catch(err => {
-    res.status(500).send({ error: err });
-  });
+    db.get(req.params.id).then(todo => {
+        res.send(todo);
+    }).catch(err => {
+        res.status(500).send({ error: err });
+    });
 });
 
 app.put('/api/todos/:id', (req, res) => {
-  db.update(req.params.id, req.body)
-    .then(todo => db.search())
-    .then(todos => res.send(todos))
-    .catch(err => {
-      res.status(500).send({ error: err });
-  });
+    db.update(req.params.id, req.body)
+        .then(todo => db.search())
+        .then(todos => res.send(todos))
+        .catch(err => {
+            res.status(500).send({ error: err });
+        });
 });
 
 app.delete('/api/todos/:id', (req, res) => {
-  db.delete(req.params.id)
-    .then(todo => db.search())
-    .then(todos => res.send(todos))
-    .catch(err => {
-    res.status(500).send({ error: err });
-  });
+    db.delete(req.params.id)
+        .then(todo => db.search())
+        .then(todos => res.send(todos))
+        .catch(err => {
+            res.status(500).send({ error: err });
+        });
 });
 
 // Activate this route to debug
@@ -165,16 +163,15 @@ app.delete('/api/todos/:id', (req, res) => {
 // });
 
 app.listen(appEnv.port, function () {
-  console.log("server starting on " + appEnv.url);
+    console.log("server starting on " + appEnv.url);
 });
 
 // Try to reconnect to the DB after 5sec
 function initDb() {
-  db.init().catch((err) => {
-    setTimeout(initDb, 5000);
-  })
+    db.init().catch((err) => {
+        setTimeout(initDb, 5000);
+    })
 }
 
 // connect to the database
 initDb();
-
